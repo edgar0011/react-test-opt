b113af41b37e59db92b83eeeef00e53854bc3b66
edgar0011 <martin.weiser@gmail.com>
2017-05-06 10:12:31 +0200

    chore(): lost test restored

---
+++ test/calcReducerSpec.js
@@ -0,0 +1,34 @@
+import calcReducer from '../app/src/reducers/calcReducer';
+import { CALCULATE_VOLATILITY, CALCULATE_PRICE } from '../app/src/config/CONSTANTS';
+
+import { expect, assert } from 'chai';
+
+describe('calcReducer', () => {
+	describe('CALCULATE_VOLATILITY', () => {
+		it('should CALCULATE_VOLATILITY', () => {
+			const initialState = {result:{volatility:0, price:0, errors:[]}};
+			const action = {
+				type: CALCULATE_VOLATILITY,
+				payload: {input:22}
+			};
+			const nextState = calcReducer(initialState, action);
+
+			expect(nextState.result.volatility).to.be.above(0.511);
+
+	  });
+  });
+
+  describe('CALCULATE_PRICE', () => {
+    it('should CALCULATE_PRICE', () => {
+      const initialState = {result:{volatility:0, price:0, errors:[]}};
+      const action = {
+        type: CALCULATE_PRICE,
+        payload: {input:0.1}
+      };
+      const nextState = calcReducer(initialState, action);
+
+      expect(nextState.result.price).to.be.above(5.0808085);
+
+    });
+  });
+});
